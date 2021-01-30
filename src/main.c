3331f3da989512966e56ca9ef5ae8cac32b311e3
ShifterBit <48891590+ShifterBit@users.noreply.github.com>
2021-01-30 22:56:37 +0000

    Set up structure for bytecode

--- src/main.c
+++ src/main.c
@@ -1,10 +1,20 @@
 #include "chunk.h"
 #include "common.h"
+#include "debug.h"
 
 int main(int argc, const char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
-  writeChunk(&chunk, OP_RETURN);
+
+  int constant = addConstant(&chunk, 1.2);
+
+  writeChunk(&chunk, OP_CONSTANT, 13);
+  writeChunk(&chunk, OP_RETURN, 42);
+  writeChunk(&chunk, OP_RETURN, 42);
+
+  disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
+
+
   return 0;
 }
