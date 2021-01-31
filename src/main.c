3d6f5cf94c8b0fc8906b80fb7ad6368037a8eea8
ShifterBit <48891590+ShifterBit@users.noreply.github.com>
2021-01-31 19:23:02 +0000

    Add binary operators

--- src/main.c
+++ src/main.c
@@ -7,13 +7,25 @@ int main(int argc, const char *argv[]) {
   initVM();
   Chunk chunk;
   initChunk(&chunk);
-
   int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
 
-  writeChunk(&chunk, OP_CONSTANT, 13);
-  writeChunk(&chunk, constant, 13);
-  writeChunk(&chunk, OP_NEGATE, 13);
-  writeChunk(&chunk, OP_RETURN, 13);
+  constant = addConstant(&chunk, 3.4);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_ADD, 123);
+
+  constant = addConstant(&chunk, 5.6);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_DIVIDE, 123);
+
+  writeChunk(&chunk, OP_NEGATE, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
 
   disassembleChunk(&chunk, "test chunk");
   interpret(&chunk);
